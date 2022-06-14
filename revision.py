dfb7ad8a74f41c4f93301bdb7b5465f652557c01
Grace-Waceke <gracewacekemwangi@gmail.com>
2022-06-14 17:42:32 +0300

    added dictionaries

--- revision.py
+++ revision.py
@@ -102,11 +102,8 @@ def shuffle(color):
 
 print(["white", "black", "yellow", "blue", "red", "green"])
 
-from cgitb import text
-from pickletools import string1
+
 from random import shuffle
-from re import L
-from unicodedata import name
 color = ["white", "black", "yellow", "blue", "red", "green"]
 shuffle(color)
 print(color)
@@ -177,3 +174,91 @@ for n in x:
         if x < smallest:
             smallest = x
 print (smallest)
+
+# # Write a Python program to create a list by concatenating a given list which range goes from 1 to n.
+# my_list = ['a', 'b']
+# n = 4
+# new_list = ['{}{}'.format(x, y) for y in range(1, n+1) for x in my_list]
+# print(new_list())
+
+# Write a Python program to find common items from two lists.
+color1 = ["red", "blue", "green", "yellow"]
+color2 = ["green", "purple", "indigo", "blue"]
+print(set(color1) & set(color2))
+
+# Write a Python script to sort (ascending and descending) a dictionary by value.
+import operator
+d = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+print('Original dictionary : ',d)
+sorted_dict = sorted(d.items(), key=operator.itemgetter(1))
+print('Dictionary in ascending order by value : ',sorted_dict)
+sorted_dict = dict( sorted(d.items(), key=operator.itemgetter(1),reverse=True))
+print('Dictionary in descending order by value : ',sorted_dict)
+
+# Write a Python program to add a key to a dictionary.
+d = {10:30, 40:34}
+print(d)
+
+d.update({60:70})
+print(d)
+
+# Write a Python program to concatenate following dictionaries to create a new one.
+dic1 = {10:20, 70:80}
+dic2 = {30:40, 90:100}
+dic3 = {50:60, 110:120}
+dic4 = {}
+for x in (dic1, dic2, dic3): dic4.update(x)
+print(dic4)
+
+# Write a Python program to check whether a given key already exists in a dictionary.
+d = {1:10, 2:20, 3:30, 4:40, 5:50,6:60}
+def key_present(x):
+    for x in d:
+        print("Key is in the dictionary")
+    else:
+        print("Key isn't in the dictionary")
+
+key_present(5)
+key_present(10)
+
+# Write a Python program to iterate over dictionaries using for loops.
+x = {"a":10, "b":20, "c":30}
+for dict_key, dict_value in x.items():
+    print(dict_key, "->", dict_value)
+
+# Write a Python script to generate and print a dictionary that contains a number (between 1 and n) in the form (x, x*x).
+n = int(input("Enter a number"))
+for x in range(1, n+1):
+    d[x]= x*x
+    print(d)
+
+    # Write a Python script to print a dictionary where the keys are numbers between 1 and 15 (both included) and the values are square of keys.
+d = dict()
+for x in range(1, 15):
+    d[x] = x **2
+    print(d)
+
+    # Write a Python script to merge two Python dictionaries.
+x1 = {'a': 100, 'b': 200}
+x2 = {'x': 300, 'y': 200}
+y = x1.copy()
+y.update(x2)
+print(y)
+
+# Create a new dict and loop over dicts, using dictionary.update() to add the key-value pairs from each one to the result.
+def merge_dictionaries(*dicts):
+    result = dict()
+    for x in dicts:
+        result.update(x)
+        return result
+
+person1 = {"Sam":37, "Waceke":25}
+person2 = {"Tamara":18, "Dylan":12}
+print("Original dictionaries")
+print(person1)
+print(person2)
+print("merge dictionaries")
+print(merge_dictionaries  (person1, person2))
+
+
+
